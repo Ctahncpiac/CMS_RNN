9354398fa10570fbb5ef5e05556a70abf202b2a9
Stanislas <134210878+Ctahncpiac@users.noreply.github.com>
2024-02-09 17:43:22 +0100

    Update data_file.py
    transforming into class

--- data_file.py
+++ data_file.py
@@ -2,30 +2,33 @@ from array import array
 from ROOT import TFile, TTree
 from data_simulation import ClusterSimulator
 
-f = TFile('tree.root', "RECREATE")
-tsgn = TTree("tsgn", "sgn")
-tbkg = TTree("tbkg", "bkg")
+class file:
+    def __init__(self):
+        pass
+    def cfile(self,name,ngen):
 
-clusSize = 10
-xsng = array('f',[0]*clusSize)
-xbkg = array('f',[0]*clusSize)
-#for i in range(clusSize):
-#    tbkg.Branch("clus_" + str(i), xbkg[i], "clus_" + str(i) + "/F")
-#    tsgn.Branch("clus_" + str(i), xsng[i], "clus_" + str(i) + "/F")
-tbkg.Branch("clus", xbkg, "xbkg[10]/F")
-tsgn.Branch("clus", xsng, "xsng[10]/F")    
+        f = TFile(str(str(name)+ '.root'), "RECREATE")
+        tsgn = TTree("tsgn", "sgn")
+        tbkg = TTree("tbkg", "bkg")
 
-simulator=ClusterSimulator()
-ngen = 10000
-for i in range(ngen):
-    onemip = simulator.generate_MIP_cluster()
-    for i in range(len(onemip)):
-        xsng[i] = onemip[i]
-    twomip = simulator.generate_2MIP_cluster()
-    for i in range(len(onemip)):
-        xbkg[i] = twomip[i]
-    tsgn.Fill()
-    tbkg.Fill()
-f.Write()
-f.Print()
-f.Close()
+        clusSize = 10
+        xsng = array('f',[0]*clusSize)
+        xbkg = array('f',[0]*clusSize)
+        #for i in range(clusSize):
+        #    tbkg.Branch("clus_" + str(i), xbkg[i], "clus_" + str(i) + "/F")
+        #    tsgn.Branch("clus_" + str(i), xsng[i], "clus_" + str(i) + "/F")
+        tbkg.Branch("clus", xbkg, "xbkg[10]/F")
+        tsgn.Branch("clus", xsng, "xsng[10]/F")    
+        simulator=ClusterSimulator()
+        for i in range(ngen):
+            onemip = simulator.generate_MIP_cluster()
+            for i in range(len(onemip)):
+                xsng[i] = onemip[i]
+            twomip = simulator.generate_2MIP_cluster()
+            for i in range(len(onemip)):
+                xbkg[i] = twomip[i]
+            tsgn.Fill()
+            tbkg.Fill()
+        f.Write()
+        f.Print()
+        f.Close()   
