0bbe47665f26c65c377dc16d4038dfa6be517101
sacharejai <132498926+sacharejai@users.noreply.github.com>
2024-02-12 12:55:38 +0100

    Update draw.py

--- draw.py
+++ draw.py
@@ -6,7 +6,7 @@ ngen = 1000
 c2 = TCanvas()
 h = TH1F('h','',10,0,10)
 for i in range(ngen):
-    simulator=ClusterSimulator("config.variables_1")
+    simulator=ClusterSimulator("config1.json")
     clus = simulator.generate_MIP_cluster()
     for i in range(10):
         h.SetBinContent(i,clus[i])
