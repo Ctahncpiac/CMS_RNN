81db60e057158a062999cb790fb3544b503f0497
Stanislas <134210878+Ctahncpiac@users.noreply.github.com>
2024-02-13 13:41:06 +0100

    Update training.py

--- training.py
+++ training.py
@@ -2,8 +2,11 @@ import ROOT
 from RNN import Classification
 from ROOT import TMVA, TFile
 
-C=Classification()
-factory=C.tmva()
+C=Classification(name='test',ngen=10000, writeOutputFile=True)
+
+factory=C.tmva(type=3 , batchSize=100, maxepochs=10, nEvts=1000, pB=0.8, pS=0.8)
+#factory=C.PyMVA(type=3 , batchSize=100, maxepochs=10, nEvts=1000, pB=0.8, pS=0.8)
+
 ## Train all methods
 factory.TrainAllMethods()
 print("nthreads  = {}".format(ROOT.GetThreadPoolSize()))
