8b006dde09108d6d9efb54120080bfbb857de3b9
Stanislas <134210878+Ctahncpiac@users.noreply.github.com>
2024-02-13 12:37:28 +0100

    Update training.py

--- training.py
+++ training.py
@@ -1,1 +1,16 @@
+import ROOT
+from RNN import Classification
+from ROOT import TMVA, TFile
 
+C=Classification()
+factory=C.tmva()
+## Train all methods
+factory.TrainAllMethods()
+print("nthreads  = {}".format(ROOT.GetThreadPoolSize()))
+        
+# ---- Evaluate all MVAs using the set of test events
+factory.TestAllMethods()
+        
+        # ----- Evaluate and compare performance of all configured MVAs
+factory.EvaluateAllMethods()
+ 
