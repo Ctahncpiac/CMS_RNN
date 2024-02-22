88849d8314b35a05f35f56111a50184c603fb2e6
sacharejai <132498926+sacharejai@users.noreply.github.com>
2024-02-22 19:12:58 +0100

    Update Trajectory_with_plot.py

--- Trajectory_with_plot.py
+++ Trajectory_with_plot.py
@@ -3,121 +3,117 @@ import matplotlib.pyplot as plt
 import random as rand
 
 class ParticleTrajectory:
-    def generate_position(self,w):
-        self.w=w
-        return rand.uniform(0,self.w)  #retoune un réel compris entre 0 et r (de manière uniforme)
+    def generate_position(self,D):
+        self.D = D
+        return rand.uniform(0, self.D)  # retourne un réel compris entre 0 et D (de manière uniforme)
     
-    def distr_theta(self):
-        # Générer une variable aléatoire uniforme entre 0 et 1
-        u = np.random.rand()                                                #pq utiliser np.random.rand alors qu'on a rand.uniform(0,1) ?
-        # Calculer l'angle theta en fonction de la distribution x^2
-        theta = np.arcsin(np.sqrt(u) * np.sin(np.pi / 3))
-        # Choisir aléatoirement la direction de l'angle
+    def distr_theta(self):                                              
+        theta = np.random.uniform(np.pi / 4, np.pi / 2)
         if np.random.rand() < 0.5:
             return np.pi / 2 - theta  # Particule partant vers la droite
         else:
             return -(np.pi / 2 - theta)  # Particule partant vers la gauche
 
-    def tracer_droites_et_rectangles(self, r, t ,w):
-        self.r=r
-        self.t=t
-        self.w=w
-        #Calculs of initial and final positions
-
-        #real coordinates
-        y_i=0
-        x_i = self.generate_position(self.w)
+    def tracer_droites_et_rectangles(self, D, d):
+        self.D = D
+        self.d = d
+        
+        plt.figure(figsize=(10, 10))
+        plt.plot([0, self.D], [0, 0], 'k')  # Première droite
+        plt.plot([0, self.D], [self.d, self.d], 'k')  # Deuxième droite
+        
+        for i in range(int(self.D) + 1):
+            plt.plot([i * self.D / 10, i * self.D / 10], [0, self.d], 'k')  # Droites perpendiculaires
+        for i in range(int(self.D)):
+            plt.fill([i * self.D / 10, (i + 1) * self.D / 10, (i + 1) * self.D / 10, i * self.D / 10], [0, 0, self.d, self.d], 'gray', alpha=0.3)
+        
+        y_i = 0
+        x_i = self.generate_position(self.D)
         angle = self.distr_theta()
-        x_f=x_i+np.tan(angle)*self.t
-        y_f=self.t
-
-        if x_f < 0 :
-            x_f=0
-            y_f=(x_f-x_i)/np.tan(angle)
-
-        elif x_f > self.w:
-
-            x_f=self.w
-            y_f=(x_f-x_i)/np.tan(angle)
+        x_f = x_i + np.tan(angle) * self.d
+        y_f = self.d
+        if x_f < 0:
+            x_f = 0
+            y_f = (x_f - x_i) / np.tan(angle)
+        elif x_f > self.D:
+            x_f = self.D
+            y_f = (x_f - x_i) / np.tan(angle)
         
-         
-        X=[x_i,x_f]
-        Y=[y_i,y_f]
-
-        #rescale coordinates 
-        x_i= x_i*(self.r/self.w)
-        x_f= x_f*(self.r/self.w) 
-
-        #Attribution segment 
-        S=[0]*self.r
-        d=abs(x_f-x_i)
-
-        if x_f == self.r:  #to ensure the case where x_f=D and int(x_i)=D-1 which will lead to d_int=1
-            x_f = self.r -1 
-
-        d_int= int(x_f)-int(x_i)
-
-        if d_int > 0: #theta > 0
-
-            S[int(x_i)]=int(x_i+1)-x_i 
-            d=d-S[int(x_i)]
-
-            j=1    
-            while d>1:
-                S[int(x_i)+j]=1.0
-                d-=1
-                j+=1
-
-            S[int(x_f)]=d    
-              
-        elif d_int < 0:  #theta < 0
-
-            S[int(x_i)]= x_i - int(x_i)
-            d=d-S[int(x_i)]
-
-            j=1
-            while d>1:
-                S[int(x_i)-j]=1.0
-                d-=1
-                j+=1
-
-            S[int(x_f)]=d 
-        else :
-            S[int(x_i)]=d     #case where x_i and x_f are in the same column
-        
-        ############
-        #Affichage graphique des colonnes
-        
-        # Initialisations pour le tracé
-        plt.plot([0, self.w], [0, 0], 'k')  # Première droite
-        plt.plot([0, self.w], [self.t, self.t], 'k')  # Deuxième droite
-        
-        # Tracer les droites et rectangles
-        for i in range(self.r+1):
-            plt.plot([i *(self.w/self.r), i *(self.w/self.r)], [0, self.t], 'k')  # Droites perpendiculaires
-
-        plt.plot(X, Y, 'r-', linewidth=2)
+        plt.plot([x_i, x_f], [y_i, y_f], 'r-', linewidth=2)
+        plt.xlabel('Distance (unité de longueur)')
+        plt.ylabel('Distance (unité de longueur)')
+        plt.title('Passage d\'une particule à travers les plans du détecteur')
+        plt.legend(['Trajectoire de la particule'])
+        plt.gca().set_aspect('equal', adjustable='box')
         plt.show()
-        ############
 
+        self.X = [x_i, x_f]
+        self.Y = [y_i, y_f]
+        self.calculate_segments(x_i, x_f, angle)
 
-        C=[0]*self.r
-        C[int(x_i)]=self.t
+    def calculate_segments(self, x_i, x_f, angle):
+        S = [0] * int(self.D)
+        d = abs(x_f - x_i)
+        
+        if x_f == self.D:  # to ensure the case where x_f=D and int(x_i)=D-1
+            x_f = self.D - 1 
 
-        if angle !=0 :
-            C = [ (i*self.w)/(self.r * np.sin(abs(angle))) for i in S]
-         
+        d_int = int(x_f) - int(x_i)
 
-        return X, S, C
-    
+        if d_int > 0: # theta > 0
+            S[int(x_i)] = int(x_i + 1) - x_i
+            d = d - S[int(x_i)]
+
+            j = 1
+            while d > 1:
+                S[int(x_i) + j] = 1.0
+                d -= 1
+                j += 1
+
+            S[int(x_f)] = d
+        elif d_int < 0:  # theta < 0
+            S[int(x_i)] = x_i - int(x_i)
+            d = d - S[int(x_i)]
+
+            j = 1
+            while d > 1:
+                S[int(x_i) - j] = 1.0
+                d -= 1
+                j += 1
+
+            S[int(x_f)] = d
+        else:
+            S[int(x_i)] = d  # case where x_i and x_f are in the same column
+
+        self.S = S
+        if angle != 0:
+            self.C = [i / np.sin(abs(angle)) for i in S]
+        else:
+            self.C = [0] * len(S)  # Avoid division by zero
+
+    def ChargeCluster(self, cross_talk_factor=0.5):
+        K = 1.5  # Appliquer le facteur de gain initial
+        C_modified = [charge * K for charge in self.C]  # Applique le facteur K à chaque charge
+
+        # Appliquer l'effet de cross-talk
+        C_with_cross_talk = [0] * len(C_modified)
+        for i in range(len(C_modified)):
+            C_with_cross_talk[i] += C_modified[i]
+            if i > 0:
+                C_with_cross_talk[i - 1] += C_modified[i] * cross_talk_factor
+            if i < len(C_modified) - 1:
+                C_with_cross_talk[i + 1] += C_modified[i] * cross_talk_factor
+        return C_with_cross_talk
 
 # Utilisation de la classe
 particle_trajectory = ParticleTrajectory()
-r = 100 # resolution
-t = 14  # epaisseur
-w = 17 #largeur
-X, S , C = particle_trajectory.tracer_droites_et_rectangles(r,t,w)
-print("Coordonées x_i - x_f :", X)
-print("Projection sur l'abscisse (unité):", S)
-print("Segmentation",C)
+D = 10 # Largeur de l'espace entre les deux plans
+d = 3.0  # Hauteur de chaque petit rectangle
+particle_trajectory.tracer_droites_et_rectangles(D, d)
+X, S, C = particle_trajectory.X, particle_trajectory.S, particle_trajectory.C
+print("Coordonnées initiale - finale :", X)
+print("Projection sur l'abscisse :", S)
+print("Segmentation", C)
 print("Distance totale parcourue :", sum(C))
+Charge_déposée = particle_trajectory.ChargeCluster(cross_talk_factor=0.1)  # Ajustez le facteur selon le besoin
+print("Charge déposée avec cross-talk :", Charge_déposée)
