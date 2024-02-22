6b4d8415507be9ff53869fe576a242ea56b16ca7
sacharejai <132498926+sacharejai@users.noreply.github.com>
2024-02-22 19:13:49 +0100

    Update data_simulation.py

--- data_simulation.py
+++ data_simulation.py
@@ -5,7 +5,7 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 import json
-import Trajectory_with_plot.py
+from Trajectory_with_plot.py import ParticleTrajectory
 
 
 rand = TRandom3(int(time.time()))
