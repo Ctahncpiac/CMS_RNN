4aa2e46f50198644617f26a0d1d0b414adbeb91a
Stanislas <134210878+Ctahncpiac@users.noreply.github.com>
2024-02-23 17:02:06 +0100

    Update draw.py

--- draw.py
+++ draw.py
@@ -1,13 +1,14 @@
 from ROOT import TH1F, TCanvas, kBlue, kRed
 from data_simulation import ClusterSimulator
+import matplotlib.pyplot as plt
 
+simulator=ClusterSimulator("config1.json")
+ngen = 10
 # draw the data simulation
 print("")
 print("------------------MIP--------------------------")
 print("")
 c2 = TCanvas()
-simulator=ClusterSimulator("config1.json")
-ngen = 100
 c2 = TCanvas()
 h = TH1F('h','',simulator.r,0,simulator.r)
 for j in range(ngen):
@@ -51,3 +52,23 @@ for j in range(ngen):
 print("")
 print("------------------END--------------------------")
 print("")
+
+
+for i in range(ngen):
+    ###########
+            
+    # Initialisations pour le tracé
+    plt.plot([0, simulator.w], [0, 0], 'k')  # Première droite
+    plt.plot([0, simulator.w], [simulator.t, simulator.t], 'k')  # Deuxième droite
+            
+    # Tracer les droites et rectangles
+    for i in range(simulator.r+1):
+        plt.plot([i *(simulator.w/simulator.r), i *(simulator.w/simulator.r)], [0, simulator.t], 'k')  # Droites perpendiculaires
+    L=simulator.charge(simulator.r,simulator.t,simulator.w,1)
+
+    plt.plot(L[0], L[1], 'r-', linewidth=2)
+    plt.xlabel('Width')
+    plt.ylabel('Thickness')
+    plt.savefig('trajectory_clus_'+str(simulator.r)+'_'+str(i)+'.png')
+    plt.show()
+    ############
